a74bb055ac24d4fcc63531ac26cf0c0bf4edd413
ankit4298 <ankit4298@gmail.com>
2021-09-26 15:07:04 +0530

    added media query for queue list responsive view

--- src/components/PlayingQueue.js
+++ src/components/PlayingQueue.js
@@ -13,7 +13,37 @@ import QueueListCard from './QueueListCard';
 const DRAWER_DIRECTION = 'right';
 const useStyles = makeStyles((theme) => ({
     list: {
-      width: '50vw',
+      '@media only screen and (min-width: 960px)' :{
+        /* styles for browsers larger than 960px; */
+        width: '50vw',
+      },
+      '@media only screen and (min-width: 1440px)' :{
+          /* styles for browsers larger than 1440px; */
+        width: '50vw',
+      },
+      '@media only screen and (min-width: 2000px)' :{
+          /* for sumo sized (mac) screens */
+        width: '50vw',
+      },
+      '@media only screen and (max-device-width: 480px)' :{
+        /* styles for mobile browsers smaller than 480px; (iPhone) */
+        width: '90vw',
+      },
+      '@media only screen and (device-width: 768px)' :{
+        /* default iPad screens */
+        width: '90vw',
+      },
+      /* different techniques for iPad screening */
+      '@media only screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)' :{
+        /* For portrait layouts only */
+        width: '90vw',
+      },
+
+      '@media only screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)' :{
+        /* For landscape layouts only */
+        width: '90vw',
+      }
+
     },
     fullList: {
       width: 'auto',
@@ -32,7 +62,7 @@ const useStyles = makeStyles((theme) => ({
 
 
     topPane: {
-      height: "100vh",
+      height: "100%",
       width: "100%",
       backgroundColor: '#dddffe',
     },
